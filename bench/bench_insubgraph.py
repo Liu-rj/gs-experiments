be1a3b8f8e18bd87baf0b1b97cebe3657c245a73
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2022-12-21 20:27:11 +0800

    benchmark fo in_subgraph

---
+++ bench/bench_insubgraph.py
@@ -0,0 +1,100 @@
+import dgl
+import gs
+import torch
+from ogb.nodeproppred import DglNodePropPredDataset
+import time
+import numpy as np
+
+_CSR = 4
+_CSC = 2
+_COO = 1
+
+torch.manual_seed(1)
+
+data = DglNodePropPredDataset(name='ogbn-products', root="../../datasets")
+graph = data[0][0]
+graph.ndata.clear()
+graph.edata.clear()
+
+graph = graph.to('cuda')
+
+indptr, indices, _ = graph.adj_sparse('csc')
+indptr = indptr.cuda()
+indices = indices.cuda()
+
+m = gs.Graph(False)
+m._CAPI_load_csc(indptr, indices)
+
+full_m = gs.Graph(False)
+full_m._CAPI_full_load_csc(indptr, indices)
+
+node_seeds = torch.randperm(graph.num_nodes())
+
+for num_seeds in [1000, 10000, 100000, 500000, 1000000]:
+    seeds = node_seeds[:num_seeds].cuda()
+    time_list = []
+
+    # DGL SpMM
+    time_list.clear()
+    for i in range(100):
+        tic = time.time()
+        subgraph = dgl.in_subgraph(graph, seeds)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    num_edges = subgraph.num_edges()
+    print(" DGL", "CSC", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
+
+    # Full
+    time_list.clear()
+    for i in range(100):
+        tic = time.time()
+        sub_full_m = full_m._CAPI_full_slicing(seeds, 0, _COO)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    print("Full", "COO", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
+
+    time_list.clear()
+    for i in range(100):
+        tic = time.time()
+        sub_full_m = full_m._CAPI_full_slicing(seeds, 0, _CSR)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    print("Full", "CSR", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
+
+    for i in range(100):
+        tic = time.time()
+        sub_full_m = full_m._CAPI_full_slicing(seeds, 0, _CSC)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    print("Full", "CSC", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
+
+    # DCSR
+    time_list.clear()
+    for i in range(100):
+        tic = time.time()
+        sub_m = m._CAPI_slicing(seeds, 0, _COO, _COO)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    print("DCSR", "COO", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
+
+    time_list.clear()
+    for i in range(100):
+        tic = time.time()
+        sub_m = m._CAPI_slicing(seeds, 0, _CSR, _CSR)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    print("DCSR", "CSR", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
+
+    for i in range(100):
+        tic = time.time()
+        sub_m = m._CAPI_slicing(seeds, 0, _CSC, _CSC)
+        torch.cuda.synchronize()
+        toc = time.time()
+        time_list.append(toc - tic)
+    print("DCSR", "CSC", num_seeds, num_edges, 1000 * np.mean(time_list[10:]))
