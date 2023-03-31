9d926341fa9110c2f2b1567ef7f316b673f7f25c
Liu-rj <11911808@mail.sustech.edu.cn>
2023-03-31 10:42:35 +0000

    reformat files

--- main_exp_sampling/complex_algorithms/ladies/matrix.py
+++ main_exp_sampling/complex_algorithms/ladies/matrix.py
@@ -14,37 +14,39 @@ def sample_w_o_relabel(P: gs.Matrix, fanouts, seeds, seeds_ptr):
     graph = P._graph
     output_node = seeds
     blocks = []
+    encoding_size = graph._CAPI_get_num_rows()
     for fanout in fanouts:
         # (batchID * num_nodes) * nodeID
         subg = graph._CAPI_batch_slicing(seeds, seeds_ptr, 0, gs._CSC, gs._COO, False)
-        # probs = subg._CAPI_sum(1, 2, gs._COO)
+        probs = subg._CAPI_sum(1, 2, gs._COO)
 
-        # neighbors = torch.unique(subg._CAPI_get_coo_rows())
-        # # int(nodeID / num_nodes)
-        # node_probs = probs[neighbors]
-        # neighbors_ptr, _ = torch.ops.gs_ops.GetBatchOffsets(neighbors, num_batches)
-        # selected, _, selected_ptr = gs.batch_list_sampling_with_probs(
-        #     neighbors, node_probs, fanout, False, neighbors_ptr)
+        neighbors = torch.unique(subg._CAPI_get_coo_rows(False))
+        # int(nodeID / num_nodes)
+        node_probs = probs[neighbors]
+        neighbors_ptr, _ = torch.ops.gs_ops.GetBatchOffsets(neighbors, num_batches, encoding_size)
+        idx, _ = torch.ops.gs_ops.batch_list_sampling_with_probs(node_probs, fanout, False, neighbors_ptr)
+        selected = neighbors[idx]
 
-        # nodes = torch.cat((subg._CAPI_get_cols(), selected)).unique()  # add self-loop
-        # subg = subg._CAPI_slicing(nodes, 1, gs._COO, gs._COO)  # Row Slicing
-        # subg = subg._CAPI_divide(probs[nodes], 1, gs._COO)
-        # subg = subg._CAPI_normalize(0, gs._COO)
+        nodes = torch.cat((subg._CAPI_get_cols(), selected)).unique() # add self-loop
+        subg = subg._CAPI_slicing(nodes, 1, gs._COO, gs._COO, False) # Row Slicing
+        subg = subg._CAPI_divide(probs[nodes], 1, gs._COO)
+        _sum = subg._CAPI_sum(0, 1, gs._COO)
+        subg = subg._CAPI_divide(_sum, 0, gs._COO)
 
-        # encoded_coo_row = subg._CAPI_get_rows()[subg._CAPI_get_coo_rows()]
-        # # nodeID - int(nodeID / num_nodes) * num_nodes
-        # coo_ptr, coo_row = torch.ops.gs_ops.GetBatchOffsets(encoded_coo_row, num_batches, graph._CAPI_get_num_rows())
-        # coo_col = seeds[subg._CAPI_get_coo_cols()]
-        # unique_tensor, unique_tensor_ptr, sub_coo_row, sub_coo_col, sub_coo_ptr = gs.BatchCOORelabel(
-        #     seeds, seeds_ptr, coo_col, coo_row, coo_ptr)
-        # seedst = torch.ops.gs_ops.SplitByOffset(seeds, seeds_ptr)
-        # unit = torch.ops.gs_ops.SplitByOffset(unique_tensor, unique_tensor_ptr)
-        # colt = torch.ops.gs_ops.SplitByOffset(sub_coo_col, coo_ptr)
-        # rowt = torch.ops.gs_ops.SplitByOffset(sub_coo_row, coo_ptr)
-        # eweight = torch.ops.gs_ops.SplitByOffset(subg.val, coo_ptr)
-        # blocks.insert(0, (seedst, unit, colt, rowt, eweight))
+        encoded_coo_row = subg._CAPI_get_rows()[subg._CAPI_get_coo_rows(False)]
+        # nodeID - int(nodeID / num_nodes) * num_nodes
+        coo_ptr, coo_row = torch.ops.gs_ops.GetBatchOffsets(encoded_coo_row, num_batches, encoding_size)
+        coo_col = seeds[subg._CAPI_get_coo_cols(False)]
+        unique_tensor, unique_tensor_ptr, sub_coo_row, sub_coo_col, sub_coo_ptr = torch.ops.gs_ops.BatchCOORelabel(
+            seeds, seeds_ptr, coo_col, coo_row, coo_ptr)
+        seedst = torch.ops.gs_ops.SplitByOffset(seeds, seeds_ptr)
+        unit = torch.ops.gs_ops.SplitByOffset(unique_tensor, unique_tensor_ptr)
+        colt = torch.ops.gs_ops.SplitByOffset(sub_coo_col, coo_ptr)
+        rowt = torch.ops.gs_ops.SplitByOffset(sub_coo_row, coo_ptr)
+        eweight = torch.ops.gs_ops.SplitByOffset(subg._CAPI_get_data('default'), coo_ptr)
+        blocks.insert(0, (seedst, unit, colt, rowt, eweight))
 
-        # seeds, seeds_ptr = unique_tensor, unique_tensor_ptr
+        seeds, seeds_ptr = unique_tensor, unique_tensor_ptr
     input_node = seeds
     return input_node, output_node, blocks
 
@@ -54,33 +56,34 @@ def sample_w_relabel(P: gs.Matrix, fanouts, seeds, seeds_ptr):
     graph = P._graph
     output_node = seeds
     blocks = []
+    encoding_size = graph._CAPI_get_num_rows()
     for fanout in fanouts:
         subg = graph._CAPI_batch_slicing(seeds, seeds_ptr, 0, gs._CSC, gs._COO, True)
         probs = subg._CAPI_sum(1, 2, gs._COO)
         num_pick = np.min([probs.numel(), fanout])
 
         # int(nodeID / num_nodes)
-        row_ptr, _ = torch.ops.gs_ops.GetBatchOffsets(subg._CAPI_get_rows(), num_batches, graph._CAPI_get_num_rows())
-        selected, _, _ = gs.batch_list_sampling_with_probs(
-            torch.arange(subg.num_rows()), probs, num_pick, False, row_ptr)
+        row_ptr, _ = torch.ops.gs_ops.GetBatchOffsets(subg._CAPI_get_rows(), num_batches, encoding_size)
+        selected, _ = torch.ops.gs_ops.batch_list_sampling_with_probs(probs, num_pick, False, row_ptr)
 
-        relabel_seeds_nodes = gs.index_search(subg._CAPI_get_rows(), subg._CAPI_get_cols())
+        relabel_seeds_nodes = torch.ops.gs_ops.index_search(subg._CAPI_get_rows(), subg._CAPI_get_cols())
         nodes = torch.cat((relabel_seeds_nodes, selected)).unique()
-        subg = subg._CAPI_slicing(nodes, 1, gs._COO, gs._COO)  # Row Slicing
+        subg = subg._CAPI_slicing(nodes, 1, gs._COO, gs._COO, False) # Row Slicing
         subg = subg._CAPI_divide(probs[nodes], 1, gs._COO)
-        subg = subg._CAPI_normalize(0, gs._COO)
+        _sum = subg._CAPI_sum(0, 1, gs._COO)
+        subg = subg._CAPI_divide(_sum, 0, gs._COO)
 
-        encoded_coo_row = subg._CAPI_get_rows()[subg._CAPI_get_coo_rows()]
+        encoded_coo_row = subg._CAPI_get_rows()[subg._CAPI_get_coo_rows(False)]
         # int(nodeID / num_nodes)
-        coo_ptr, coo_row = torch.ops.gs_ops.GetBatchOffsets(encoded_coo_row, num_batches, graph._CAPI_get_num_rows())
-        coo_col = seeds[subg._CAPI_get_coo_cols()]
-        unique_tensor, unique_tensor_ptr, sub_coo_row, sub_coo_col, sub_coo_ptr = gs.BatchCOORelabel(
+        coo_ptr, coo_row = torch.ops.gs_ops.GetBatchOffsets(encoded_coo_row, num_batches, encoding_size)
+        coo_col = seeds[subg._CAPI_get_coo_cols(False)]
+        unique_tensor, unique_tensor_ptr, sub_coo_row, sub_coo_col, sub_coo_ptr = torch.ops.gs_ops.BatchCOORelabel(
             seeds, seeds_ptr, coo_row, coo_col, coo_ptr)
         seedst = torch.ops.gs_ops.SplitByOffset(seeds, seeds_ptr)
         unit = torch.ops.gs_ops.SplitByOffset(unique_tensor, unique_tensor_ptr)
         colt = torch.ops.gs_ops.SplitByOffset(sub_coo_col, coo_ptr)
         rowt = torch.ops.gs_ops.SplitByOffset(sub_coo_row, coo_ptr)
-        eweight = torch.ops.gs_ops.SplitByOffset(subg.val, coo_ptr)
+        eweight = torch.ops.gs_ops.SplitByOffset(subg._CAPI_get_data('default'), coo_ptr)
         blocks.insert(0, (seedst, unit, colt, rowt, eweight))
 
         seeds, seeds_ptr = unique_tensor, unique_tensor_ptr
@@ -208,7 +211,7 @@ def train(dataset, args):
     m._graph._CAPI_set_data(weight)
     print("Check load successfully:", m._graph._CAPI_metadata(), '\n')
 
-    n_epoch = 11
+    n_epoch = 6
     benchmark_w_o_relabel(args, m, train_nid, fanouts, n_epoch)
     benchmark_w_relabel(args, m, train_nid, fanouts, n_epoch)
 
