3e0cc0814f1b54dabab104495034bde5b7fab443
DanielMao1 <daniel4006695539@gmail.com>
2023-04-10 13:47:45 +0000

    rename folder

---
+++ main_exp_sampling/simple_algorithms/graphsage/run.sh
@@ -0,0 +1,3 @@
+python graphsage_matrix.py --dataset=friendster  --data-type=int --use-uva --device=cpu
+# python graphsage_matrix.py --dataset=friendster  --data-type=long --use-uva --device=cpu
+
---
+++ main_exp_sampling/simple_algorithms/node2vec/run.sh
@@ -0,0 +1,4 @@
+python node2vec_dgl.py --dataset=products --num-workers=4 --device=cpu
+python node2vec_dgl.py --dataset=livejournal --num-workers=4 --device=cpu
+python node2vec_dgl.py --dataset=papers100m --num-workers=4 --device=cpu
+python node2vec_dgl.py --dataset=friendster --num-workers=4 --device=cpu
